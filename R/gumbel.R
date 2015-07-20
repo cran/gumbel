22c3c7e2dbd2d2eb6e91a6c4556e6ad68d49ce96
Christophe Dutang <christophe.dutang@ensimag.fr>
2015-07-20 00:00:00 +0000

    version 1.10-1

--- R/gumbel.R
+++ R/gumbel.R
@@ -1,6 +1,6 @@
 #############################################################################
 #   Copyright (c) 2008 Anne-Lise Caillat, Christophe Dutang,                                                                #
-#   Véronique Larrieu and Triet Nguyen                                                                                                      #
+#   Veronique Larrieu and Triet Nguyen                                                                                                      #
 #                                                                                                                                                                         #
 #   This program is free software; you can redistribute it and/or modify                                               #
 #   it under the terms of the GNU General Public License as published by                                         #
